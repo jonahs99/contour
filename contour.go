0b377344e714b7a2f614ebd99f967f4e2271dfe4
Jonah Simpson <jonahs99@gmail.com>
2018-03-23 19:44:37 -0400

    interesting progress

--- contour.go
+++ contour.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"image/color"
 	"image/png"
 	"math"
@@ -55,26 +56,54 @@ func drawPoly(ctx *gg.Context, p poly) {
 	ctx.Stroke()
 }
 
+func downSample(img image.Image, n int) image.Image {
+	bounds := img.Bounds()
+	w, h := bounds.Max.X, bounds.Max.Y
+	n2 := uint32(n * n)
+	out := image.NewRGBA(image.Rect(0, 0, w/n, h/n))
+	for y := 0; y < h/n; y++ {
+		for x := 0; x < w/n; x++ {
+			var r, g, b uint32
+			for i := 0; i < n; i++ {
+				for j := 0; j < n; j++ {
+					sample := img.At(x*n+j, y*n+i)
+					sr, sg, sb, _ := sample.RGBA()
+					r += sr
+					g += sg
+					b += sb
+				}
+			}
+			r /= n2 * 256
+			g /= n2 * 256
+			b /= n2 * 256
+			c := color.RGBA{uint8(r), uint8(g), uint8(b), 255}
+			out.SetRGBA(x, y, c)
+		}
+	}
+	return out
+}
+
 func main() {
 	w, h := 600.0, 600.0
 	ctx := gg.NewContext(int(w), int(h))
 	ctx.Translate(w/2, h/2)
+	ctx.Scale(2, 2)
 
 	ctx.SetColor(color.White)
 	ctx.Clear()
 
 	contours := []poly{circle(12, 9)}
 
-	targetDist := 24.0
+	targetDist := 5.0
 
-	P, D := 0.02, -0.005
+	P, D := 0.016, -0.08
 
-	nContours := 30
+	nContours := 32
 
 	for k := 1; k < nContours; k++ {
 		follow := contours[len(contours)-1]
 
-		start := vec.NewXY(12+float64(k)*targetDist, 0)
+		start := vec.NewXY(12+float64(k)*targetDist*1.2, 0)
 		particle := start
 		vel := vec.NewXY(0, -2)
 
@@ -111,7 +140,11 @@ func main() {
 
 			particle.Add(vel)
 
-			if j > 10 && vec.Dist(particle, start) < 12 {
+			//if j > 10 && vec.Dist(particle, start) < 4 {
+			//	break
+			//}
+			theta := vec.Theta(particle)
+			if j > 10 && theta > 0 && theta < 0.04 {
 				break
 			}
 
@@ -123,17 +156,23 @@ func main() {
 
 		fmt.Printf("%d contours drawn.\n", k)
 
-		lineWidth := 2.0
-		minWidth := 0.2
-		for i, c := range contours {
-			ctx.SetLineWidth(float64(nContours-i)/float64(nContours)*lineWidth + minWidth)
-			drawPoly(ctx, c)
-		}
-
-		img := ctx.Image()
-		path := fmt.Sprintf("out-%d.png", k)
-		file, _ := os.Create(path)
-		png.Encode(file, img)
 	}
+	lineWidth := 2.0
+	//minWidth := 0.2
+	ctx.SetLineWidth(lineWidth)
+	for _, c := range contours {
+		//ctx.SetLineWidth(float64(nContours-i)/float64(nContours)*lineWidth + minWidth)
+		drawPoly(ctx, c)
+	}
+
+	img := ctx.Image()
+	path := fmt.Sprintf("out-%d.png", 0)
+	file, _ := os.Create(path)
+	png.Encode(file, img)
+
+	img = downSample(img, 2)
+	path = fmt.Sprintf("aa-%d.png", 0)
+	file, _ = os.Create(path)
+	png.Encode(file, img)
 
 }
